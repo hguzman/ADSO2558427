3d7ab5489b46eff514c4cacb8895ac0a4eea28bb
cristian pacheco <110956180+Cristian-pacheco@users.noreply.github.com>
2022-11-27 18:31:15 -0500

    dr

---
+++ aprendices/cristianpacheco/cristian/template.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'user.dart';
+
+class Template extends StatelessWidget {
+  const Template({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 15.0),
+        Text('${user.nombre!} ${user.apellido}'),
+        SizedBox(height: 15.0),
+        Image(
+          image: NetworkImage(user.avatar!),
+        ),
+        SizedBox(height: 15.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.email,
+              color: Colors.black,
+              size: 15.0),
+            Text(':'),
+            Text(user.email!),
+          ]
+        ),
+      ],);
+  }
+}
